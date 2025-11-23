e6db978391582a7ce96faedecd3405cb33cf9f2a
Carlos Arredondo <carlos.arredondoc@usm.cl>
2025-11-23 20:53:21 -0300

    new version, new comands

--- python-client/generate_proto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script para generar los archivos Python desde .proto
-
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. proto/chat.proto
-
-echo "Proto files generated successfully!"
